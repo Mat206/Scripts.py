77415fca5f0ef3332edc5c437c79818c95d8c104
Oscar Llerena <63006778+Mat206@users.noreply.github.com>
2020-07-10 11:57:28 -0500

    Update Myrange.py

--- Myrange.py
+++ Myrange.py
@@ -1,15 +1,20 @@
+#Script that works like range() funtion in python, created as an iterator
+
 class rnge:
+    #Init of properties in the class
     def __init__(self,min,max,step=1):
         print("__Init__")
         self.step = step
         self.max = max
         self.i = min
         self.aux = 0
-
+       
+    #Pass as an iterator
     def __iter__(self):
         print("__iter__")
         return self
     
+    #Logic of process
     def __next__(self):
         print("__next___")
         self.aux = self.i
@@ -17,7 +22,8 @@ class rnge:
         if self.aux > self.max:
             raise StopIteration
         return self.aux 
-
+    
+#Use example
 for i in rnge(0,13):
     print(i)
         
